84ab324e0c8b52905d8af94f4bedd74fbeb716f0
Erdodif <erdodif@gmail.com>
2021-10-05 14:38:22 +0200

    Listaelem elkészítése és megjelenítése

--- brain.js
+++ brain.js
@@ -1,4 +1,7 @@
 class Film{
+    #nev
+    #rendezo
+    #kiadasEv
     constructor(nev, rendezo, kiadasEv){
         if (isNaN(kiadasEv)){
             throw new Error("A kiadás éve nem szám (╯ ͡❛ ⏥ ͡❛)╯┻━┻")
@@ -19,15 +22,55 @@ class Film{
 }
 
 let filmTomb = []
+
+function getValueById(id){
+    return document.getElementById(id).value
+}
+
+function setValueById(id,value){
+    document.getElementById(id).value = value
+}
+
 function filmHozzaAd(){
-    let nev = document.getElementById("filmNev").value
-    let rendezo = document.getElementById("filmRendezo").value
-    let kiadasEv = document.getElementById("filmKiadasEv").value
+    let nev = getValueById("filmNev")
+    let rendezo = getValueById("filmRendezo")
+    let kiadasEv = getValueById("filmKiadasEv")
     try{
-        let film = Film(nev,rendezo,kiadasEv)
+        let film = new Film(nev,rendezo,kiadasEv)
         filmTomb.push(film)
     }
     catch(e){
         console.error(e)
     }
+    setValueById("filmNev","")
+    setValueById("filmRendezo","")
+    setValueById("filmKiadasEv","")
+    kilistaz()
+}
+
+function kartyasit(film){
+    return `
+    <div class="kartya">
+        <div class="cim">
+            ${film.getNev()}
+        </div>
+        <div>
+            Rendező: ${film.getRendezo()}
+            <br/>
+            Kiadás éve: ${film.getKiadasEv()}
+        </div>
+    </div>\n`
+}
+
+function kilistaz(){
+    let lista = document.getElementById("lista")
+    let kiad = ""
+    for (const film of filmTomb){
+        kiad += kartyasit(film) 
+    }
+    lista.innerHTML = kiad
+}
+
+function init(){
+    document.getElementById("gomb_Hozzaad").addEventListener("click",filmHozzaAd)
 }
